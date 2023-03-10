c1f0975df9249266c6bdb0d9ac0f7bef5aac5eb5
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-10 18:41:37 +0700

    add setnull

--- src/OrderService.Infrastructure/Data/Config/ChatConfiguration.cs
+++ src/OrderService.Infrastructure/Data/Config/ChatConfiguration.cs
@@ -5,10 +5,10 @@ using OrderService.Core.ChatAggregate;
 namespace OrderService.Infrastructure.Data.Config;
 public class ChatConfiguration : IEntityTypeConfiguration<Chat>
 {
-  public void Configure(EntityTypeBuilder<Contributor> builder)
+  public void Configure(EntityTypeBuilder<Chat> builder)
   {
-    builder.Property(p => p.Name)
-        .HasMaxLength(100)
-        .IsRequired();
+    builder.HasOne(c => c.employee).WithMany().OnDelete(DeleteBehavior.SetNull);
+    builder.HasOne(c => c.customer).WithMany().OnDelete(DeleteBehavior.SetNull);
+
   }
 }
