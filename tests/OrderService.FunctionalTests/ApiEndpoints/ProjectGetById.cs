734d39d5498cf6b64aacf5cd7904c04132ee38af
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-14 15:14:06 +0700

    remove sqlite files and add example test

--- tests/OrderService.FunctionalTests/ApiEndpoints/ProjectGetById.cs
+++ tests/OrderService.FunctionalTests/ApiEndpoints/ProjectGetById.cs
@@ -21,7 +21,6 @@ public class ProjectGetById : IClassFixture<CustomWebApplicationFactory<WebMarke
     var result = await _client.GetAndDeserializeAsync<GetProjectByIdResponse>(GetProjectByIdRequest.BuildRoute(1));
 
     Assert.Equal(1, result.Id);
-    Assert.Equal(SeedData.TestProject1.Name, result.Name);
     Assert.Equal(3, result.Items.Count);
   }
 
---
+++ tests/OrderService.UnitTests/Core/ProductCatalogAggregate/ProductCatalogConstructor.cs
@@ -0,0 +1,41 @@
+﻿using OrderService.Core.ProductAggregate;
+using Xunit;
+
+namespace OrderService.UnitTests.Core.ProductCatalogAggregate;
+public class ProductCatalogConstructor
+{
+  private string _categoryName = "category name";
+  private ProductCategory? _productCategory;
+
+  private ProductCategory CreateProductCategory()
+  {
+    return new ProductCategory(_categoryName);
+  }
+
+  [Fact]
+  public void InitializesName()
+  {
+    _productCategory = CreateProductCategory();
+    Assert.Equal(_categoryName, _productCategory.productCategoryName);
+  }
+
+  [Fact]
+  public void ThrowsExceptionGivenNullName()
+  {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+    Action action = () => _productCategory = new ProductCategory(null);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+    var ex = Assert.Throws<ArgumentNullException>(() => action());
+
+    Assert.Equal("productCategoryName", ex.ParamName);
+  }
+
+  [Fact]
+  public void ThrowsExceptionGivenEmptyName()
+  {
+    Action action = () => _productCategory = new ProductCategory("");
+    var ex = Assert.Throws<ArgumentException>(() => action());
+
+    Assert.Equal("productCategoryName", ex.ParamName);
+  }
+}
