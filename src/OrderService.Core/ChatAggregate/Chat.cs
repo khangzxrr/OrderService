d7dbc013eab9f5086f3c6a81099788dc5ebb63e6
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-16 05:47:02 +0700

    Simlify chat table

--- src/OrderService.Core/ChatAggregate/Chat.cs
+++ src/OrderService.Core/ChatAggregate/Chat.cs
@@ -7,17 +7,10 @@ namespace OrderService.Core.ChatAggregate;
 public class Chat : EntityBase, IAggregateRoot
 {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-  public User customer { get; private set; }
-
   public User employee { get; private set; }
 
   private List<ChatMessage> _chatMessages = new List<ChatMessage>();
   public IReadOnlyCollection<ChatMessage> chatMessages => _chatMessages.AsReadOnly();
-
-  public void SetCustomer(User customer)
-  {
-    this.customer = Guard.Against.Null(customer);
-  }
 
   public void SetEmployee(User employee) {
     this.employee = Guard.Against.Null(employee);
