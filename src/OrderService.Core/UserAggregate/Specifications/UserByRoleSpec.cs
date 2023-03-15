d9d94f3d05f1d24c2646150fa6b0cd7a48581ce9
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-15 21:15:04 +0700

    role based specification

---
+++ src/OrderService.Core/UserAggregate/Specifications/UserByRoleSpec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.Specification;
+
+namespace OrderService.Core.UserAggregate.Specifications;
+public class UserByRoleSpec: Specification<User>, ISingleResultSpecification
+{
+  public UserByRoleSpec(RoleEnum role)
+  {
+    Query
+      .Include(u => u.role)
+      .Where(u => u.role.roleName == role.ToString());
+  }
+}
