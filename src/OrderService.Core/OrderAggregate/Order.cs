f4332fb0b1baa72feb72a95e27397040c9bcc249
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-15 21:13:26 +0700

    simplify additionalCost

--- src/OrderService.Core/OrderAggregate/Order.cs
+++ src/OrderService.Core/OrderAggregate/Order.cs
@@ -7,7 +7,7 @@ using OrderService.SharedKernel.Interfaces;
 namespace OrderService.Core.OrderAggregate;
 public class Order : EntityBase, IAggregateRoot
 {
-  public Chat chat { get; set; }
+  public Chat chat { get; private set; }
   public DateTime orderDate { get; }
   public OrderStatus status { get; }
   public string orderDescription { get; }
@@ -29,27 +29,27 @@ public class Order : EntityBase, IAggregateRoot
     _orderDetails.Add(orderDetail);
   }
 
-
-
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
   public Order(
-    DateTime orderDate, 
-    OrderStatus status,
     string orderDescription,
     string customerDescription,
     string deliveryAddress,
-    string contactPhonenumber,
-    int shippingEstimatedDays,
-    float price)
+    string contactPhonenumber)
   {
-    this.orderDate = Guard.Against.Null(orderDate, nameof(orderDate));
-    this.status = Guard.Against.Null(status, nameof(status));
+    orderDate = DateTime.Now;
+    status = OrderStatus.noPayYet;
     this.orderDescription = Guard.Against.NullOrEmpty(orderDescription, nameof(orderDescription));
     this.customerDescription = Guard.Against.NullOrEmpty(customerDescription, nameof(customerDescription));
     this.deliveryAddress = Guard.Against.NullOrEmpty(deliveryAddress, nameof(deliveryAddress));
     this.contactPhonenumber = Guard.Against.NullOrEmpty(contactPhonenumber, nameof(contactPhonenumber));
-    this.shippingEstimatedDays = Guard.Against.Negative(shippingEstimatedDays, nameof(shippingEstimatedDays));
-    this.price = Guard.Against.Negative(price, nameof(price));
+
+    shippingEstimatedDays = 30;
+    price = 0;
+  }
+
+  public void SetChat(Chat chat)
+  {
+    this.chat = Guard.Against.Null(chat);
   }
 
 }
--- src/OrderService.Core/OrderAggregate/OrderDetail.cs
+++ src/OrderService.Core/OrderAggregate/OrderDetail.cs
@@ -1,29 +1,29 @@
 ﻿using Ardalis.GuardClauses;
 using OrderService.Core.ProductAggregate;
 using OrderService.SharedKernel;
-using OrderService.SharedKernel.Interfaces;
 
 namespace OrderService.Core.OrderAggregate;
 public class OrderDetail : EntityBase
 {
  #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-  public ProductHistory product { get; private set; }
+  public ProductHistory productHistory { get; private set; }
 
-  public AdditionalCost additionalCost { get; private set; }
+  public float additionalCost { get; private set; }
 
-  private List<ProductReturn> _productReturns = new List<ProductReturn>();
-  public IReadOnlyCollection<ProductReturn> productReturns => _productReturns;
   public float processCost { get; private set; }
   public int quantity { get; private set; }
 
+  private List<ProductReturn> _productReturns = new List<ProductReturn>();
+  public IReadOnlyCollection<ProductReturn> productReturns => _productReturns;
+
   public void setProduct(ProductHistory product)
   {
-    this.product = Guard.Against.Null(product);
+    productHistory = Guard.Against.Null(product);
   }
 
-  public void setAdditionalCost(AdditionalCost additionalCost)
+  public void setAdditionalCost(float additionalCost)
   {
-    this.additionalCost = Guard.Against.Null(additionalCost);
+    this.additionalCost = Guard.Against.Negative(additionalCost);
   }
 
   public void setProcessCost(float processCost)
