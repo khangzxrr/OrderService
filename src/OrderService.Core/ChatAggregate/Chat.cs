b9f3fa647faf5c4ae1559b17f2ccef2c7575d5ad
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-15 21:12:36 +0700

    add setter for chat

--- src/OrderService.Core/ChatAggregate/Chat.cs
+++ src/OrderService.Core/ChatAggregate/Chat.cs
@@ -7,11 +7,22 @@ namespace OrderService.Core.ChatAggregate;
 public class Chat : EntityBase, IAggregateRoot
 {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-  public User? customer { get; set; }
+  public User customer { get; private set; }
 
-  public User? employee { get; set; }
+  public User employee { get; private set; }
 
   private List<ChatMessage> _chatMessages = new List<ChatMessage>();
   public IReadOnlyCollection<ChatMessage> chatMessages => _chatMessages.AsReadOnly();
+
+  public void SetCustomer(User customer)
+  {
+    this.customer = Guard.Against.Null(customer);
+  }
+
+  public void SetEmployee(User employee) {
+    this.employee = Guard.Against.Null(employee);
+  }
+
+
 
 }
