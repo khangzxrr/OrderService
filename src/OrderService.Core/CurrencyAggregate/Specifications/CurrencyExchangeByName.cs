91f56e65f9e63adfca92b9254d7a34a9fbcdc5ef
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-15 19:57:05 +0700

    current specification

---
+++ src/OrderService.Core/CurrencyAggregate/Specifications/CurrencyExchangeByName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.Specification;
+
+namespace OrderService.Core.CurrencyAggregate.Specifications;
+public class CurrencyExchangeByName : Specification<CurrencyExchange>, ISingleResultSpecification
+{
+  public CurrencyExchangeByName(string name)
+  {
+    Query
+      .Where(c => c.fromCurrency == name);
+  }
+}
