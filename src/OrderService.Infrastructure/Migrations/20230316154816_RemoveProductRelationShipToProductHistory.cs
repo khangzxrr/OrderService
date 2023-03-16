e07d4fe2cf5de06952fb4b27aec5bfbc0afc7ac0
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-16 22:51:49 +0700

    remove relationship between product and product history

---
+++ src/OrderService.Infrastructure/Migrations/20230316154816_RemoveProductRelationShipToProductHistory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveProductRelationShipToProductHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductHistory_Product_ProductId",
+                table: "ProductHistory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductHistory_ProductId",
+                table: "ProductHistory");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "ProductHistory");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "ProductHistory",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductHistory_ProductId",
+                table: "ProductHistory",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductHistory_Product_ProductId",
+                table: "ProductHistory",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id");
+        }
+    }
+}
