b260e2e15c6761d02a7d35013dbb31859773a473
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-16 22:52:05 +0700

    password demo 123123

--- src/OrderService.Web/SeedData.cs
+++ src/OrderService.Web/SeedData.cs
@@ -36,7 +36,7 @@ public static class SeedData
     var employee = new User("employee@fastship.com", "b45cffe084dd3d20d928bee85e7b0f21", "123123", "Khang", "Ngoc", DateTime.Now, "159 aaa bbb");
     employee.setRole(dbContext.Roles.Where(r => r.roleName == RoleEnum.EMPLOYEE.ToString()).First());
 
-    var customer = new User("customer@gmail.com", "b45cffe084dd3d20d928bee85e7b0f21", "123123", "Khang", "Ngoc", DateTime.Now, "159 aaa bbb");
+    var customer = new User("customer@gmail.com", "4297f44b13955235245b2497399d7a93", "123123", "Khang", "Ngoc", DateTime.Now, "159 aaa bbb");
     customer.setRole(dbContext.Roles.Where(r => r.roleName == RoleEnum.CUSTOMER.ToString()).First());
 
     dbContext.Users.Add(employee);
