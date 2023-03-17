03736e8a381926273318ea1b415a6fff9edffa0d
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-17 17:46:36 +0700

    add transactional field in payment migration

---
+++ src/OrderService.Infrastructure/Migrations/20230317083600_AddTransactionalIdToOrderPayment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTransactionalIdToOrderPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "transactionalId",
+                table: "OrderPayment",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "transactionalId",
+                table: "OrderPayment");
+        }
+    }
+}
