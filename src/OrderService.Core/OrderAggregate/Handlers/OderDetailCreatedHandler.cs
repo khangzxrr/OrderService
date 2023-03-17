f723a829c8677bb7054b547227853aac6c88aee1
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-17 17:44:21 +0700

    order detail created handler to calculate order total price, orderpayment to update chat noti and order status

---
+++ src/OrderService.Core/OrderAggregate/Handlers/OderDetailCreatedHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using OrderService.Core.CurrencyAggregate;
+using OrderService.Core.CurrencyAggregate.Specifications;
+using OrderService.Core.OrderAggregate.Events;
+using OrderService.Core.OrderAggregate.Specifications;
+using OrderService.SharedKernel.Interfaces;
+
+namespace OrderService.Core.OrderAggregate.Handlers;
+public class OderDetailCreatedHandler : INotificationHandler<OrderDetailCreatedEvent>
+{
+  private readonly IRepository<Order> _repository;
+  private readonly IRepository<CurrencyExchange> _currencyExchangeRepository;
+  public OderDetailCreatedHandler(IRepository<Order> repository, IRepository<CurrencyExchange> currencyExchangeRepository)
+  {
+    _repository = repository;
+    _currencyExchangeRepository = currencyExchangeRepository;
+  }
+
+  public async Task Handle(OrderDetailCreatedEvent notification, CancellationToken cancellationToken)
+  {
+    var spec = new OrderById(notification.OrderId);
+    var order = await _repository.FirstOrDefaultAsync(spec);
+
+    var totalCost = 0.0f;
+
+    var currencySpec = new CurrencyExchangeByName("US");
+    var currency = await _currencyExchangeRepository.FirstOrDefaultAsync(currencySpec);
+    if (currency == null)
+    {
+      throw new NullReferenceException("currency is null");
+    }
+
+    foreach (var orderDetail in order!.orderDetails)
+    {
+      totalCost += orderDetail.shipCost;
+      totalCost += orderDetail.productCost * orderDetail.quantity;
+      totalCost += orderDetail.additionalCost;
+      totalCost += orderDetail.processCost;
+    }
+
+    order.price = totalCost * currency!.rate;
+    await _repository.SaveChangesAsync();
+  }
+}
---
+++ src/OrderService.Core/OrderAggregate/Handlers/OrderPaymentCreatedHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using OrderService.Core.OrderAggregate.Events;
+using OrderService.Core.OrderAggregate.Specifications;
+using OrderService.SharedKernel.Interfaces;
+
+namespace OrderService.Core.OrderAggregate.Handlers;
+public class OrderPaymentCreatedHandler : INotificationHandler<OrderPaymentCreatedEvent>
+{
+
+
+  private readonly IRepository<Order> _repository;
+
+  public OrderPaymentCreatedHandler(IRepository<Order> repository)
+  {
+    _repository = repository;
+  }
+
+  public async Task Handle(OrderPaymentCreatedEvent notification, CancellationToken cancellationToken)
+  {
+    var orderSpec = new OrderById(notification.OrderId);
+    var order = await _repository.FirstOrDefaultAsync(orderSpec);
+
+    var orderPayment = order!.orderPayments.Where(op => op.Id == notification.PaymentId).FirstOrDefault();
+
+    order.chat.AddNewNotifiChatMessage(
+      $"Payment successfully, amount: {orderPayment!.paymentCost} at {orderPayment.paymentDate.ToString("HH:ss dd/MM/yyyy")}");
+
+    if (orderPayment.paymentStatus == PaymentStatus.firstPayment)
+    {
+      order.SetStatus(OrderStatus.waitingToOrderFromSeller);
+    }
+
+    await _repository.SaveChangesAsync();
+  }
+}
