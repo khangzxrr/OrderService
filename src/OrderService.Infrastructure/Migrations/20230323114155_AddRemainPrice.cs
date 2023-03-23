9c9a1c5fe31435cd50647c5a1f44f195797f9660
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-23 22:42:47 +0700

    add remain cost migration

---
+++ src/OrderService.Infrastructure/Migrations/20230323114155_AddRemainPrice.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRemainPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_Users_UserId",
+                table: "Order");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Order",
+                newName: "userId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Order_UserId",
+                table: "Order",
+                newName: "IX_Order_userId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "userId",
+                table: "Order",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "remainCost",
+                table: "Order",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Users_userId",
+                table: "Order",
+                column: "userId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Order_Users_userId",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "remainCost",
+                table: "Order");
+
+            migrationBuilder.RenameColumn(
+                name: "userId",
+                table: "Order",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Order_userId",
+                table: "Order",
+                newName: "IX_Order_UserId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Order",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Order_Users_UserId",
+                table: "Order",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
