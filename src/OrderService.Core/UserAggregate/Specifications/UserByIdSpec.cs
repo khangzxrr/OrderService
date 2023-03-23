d42ca83bbd628fe5ffd4b7220ab4b1b56bc113fc
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-23 17:00:12 +0700

    add user by Id spec

---
+++ src/OrderService.Core/UserAggregate/Specifications/UserByIdSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+
+namespace OrderService.Core.UserAggregate.Specifications;
+public class UserByIdSpec : Specification<User>, ISingleResultSpecification
+{
+  public UserByIdSpec(int userId)
+  {
+    Query
+      .Where(u => u.Id == userId)
+        .Include(u => u.orders);
+  }
+}
