8d6c7858bc22abfb21a706aa982a8bea845c5be7
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-03-28 03:25:06 +0700

    add phoneNumber migration

---
+++ src/OrderService.Infrastructure/Migrations/20230327200829_AddPhoneNumber.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhoneNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "phoneNumber",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "phoneNumber",
+                table: "Users");
+        }
+    }
+}
