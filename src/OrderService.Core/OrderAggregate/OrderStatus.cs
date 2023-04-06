dd88b830b30fb72d4202c504108fc0bb04b3bba3
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-04-07 01:29:54 +0700

    modify shipping status

--- src/OrderService.Core/OrderAggregate/OrderStatus.cs
+++ src/OrderService.Core/OrderAggregate/OrderStatus.cs
@@ -7,8 +7,9 @@ public class OrderStatus : SmartEnum<OrderStatus>
   public static readonly OrderStatus waitingToOrderFromSeller = new(nameof(waitingToOrderFromSeller), 1);
   public static readonly OrderStatus orderingFromSeller = new(nameof(orderingFromSeller), 2);
   public static readonly OrderStatus deliverFromUsToVN = new(nameof(deliverFromUsToVN), 3);
-  public static readonly OrderStatus deliverToCustomer = new(nameof(deliverToCustomer), 4);
-  public static readonly OrderStatus finished = new(nameof(finished), 5);
+  public static readonly OrderStatus inVNwarehouse = new(nameof(inVNwarehouse), 4);
+  public static readonly OrderStatus deliverToCustomer = new(nameof(deliverToCustomer), 5);
+  public static readonly OrderStatus finished = new(nameof(finished), 6);
 
   public OrderStatus(string name, int value): base(name, value) { }
 }
--- src/OrderService.Core/OrderAggregate/ShippingStatus.cs
+++ src/OrderService.Core/OrderAggregate/ShippingStatus.cs
@@ -3,10 +3,8 @@
 namespace OrderService.Core.OrderAggregate;
 public class ShippingStatus : SmartEnum<ShippingStatus>
 {
-  public static readonly ShippingStatus inUSwarehouse = new(nameof(inUSwarehouse), 0);
-  public static readonly ShippingStatus shippingFromUsToVn = new(nameof(shippingFromUsToVn), 1);
-  public static readonly ShippingStatus inVNwarehouse = new(nameof(inVNwarehouse), 2);
-  public static readonly ShippingStatus shippingToCustomer= new(nameof(shippingToCustomer), 3);
+  public static readonly ShippingStatus inVNwarehouse = new(nameof(inVNwarehouse), 0);
+  public static readonly ShippingStatus shippingToCustomer = new(nameof(shippingToCustomer), 1);
 
   public ShippingStatus(string name, int value) : base(name, value) { }
 }
