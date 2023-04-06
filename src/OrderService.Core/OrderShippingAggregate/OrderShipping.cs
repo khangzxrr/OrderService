246bdf8d2fad106fa636bbd3ab0a679712635fee
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-04-07 01:30:15 +0700

    order shipping now has reference foreign key

--- src/OrderService.Core/OrderShippingAggregate/OrderShipping.cs
+++ src/OrderService.Core/OrderShippingAggregate/OrderShipping.cs
@@ -1,4 +1,5 @@
-﻿using OrderService.Core.OrderAggregate;
+﻿using Ardalis.GuardClauses;
+using OrderService.Core.OrderAggregate;
 using OrderService.Core.ShipperAggregate;
 using OrderService.SharedKernel;
 using OrderService.SharedKernel.Interfaces;
@@ -7,11 +8,39 @@ namespace OrderService.Core.OrderShippingAggregate;
 public class OrderShipping : EntityBase, IAggregateRoot
 {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-  public Order order { get; set; }
-  public Shipper shipper { get; set; }
-  public bool shippingUsing3rd { get; set; }
-  public OrderShippingStatus orderShippingStatus { get; set; }
-  public string shippingDescription { get; set; }
-  public string signatureImageUrl { get; set; }
+  public int orderId { get; private set; }
+  public Order order { get; private set; }
+
+  public int shipperId { get; private set; }
+  public Shipper shipper { get; private set; }
+
+
+  public bool shippingUsing3rd { get; private set; }
+
+  public OrderShippingStatus orderShippingStatus { get; private set; }
+
+  public string shippingDescription { get; private set; }
+
+  public string signatureImageUrl { get; private set; }
+
+  public OrderShipping(bool shippingUsing3rd, string shippingDescription)
+  {
+    this.shippingUsing3rd = shippingUsing3rd;
+    this.shippingDescription = shippingDescription;
+    this.signatureImageUrl = "";
+    orderShippingStatus = OrderShippingStatus.inWarehouse;
+  }
+
+
+  public void setOrder(int orderId)
+  {
+    this.orderId = orderId;
+  }
+
+  public void setShipper(Shipper shipper)
+  {
+    this.shipper = Guard.Against.Null(shipper);
+  }
+
 
 }
