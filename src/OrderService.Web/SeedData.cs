4aae6fbb95d367c397e99e705fea04f0d5ea7779
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-04-07 01:33:00 +0700

    add shipper seed data

--- src/OrderService.Web/SeedData.cs
+++ src/OrderService.Web/SeedData.cs
@@ -3,6 +3,7 @@ using OrderService.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using OrderService.Core.UserAggregate;
 using OrderService.Core.CurrencyAggregate;
+using OrderService.Core.ShipperAggregate;
 
 namespace OrderService.Web;
 
@@ -10,6 +11,9 @@ public static class SeedData
 {
 
   public static readonly User employee = new User("employee@fastship.com", "091092211", "b45cffe084dd3d20d928bee85e7b0f21", "123123", "Khang", "Ngoc", DateTime.Now, "159 aaa bbb");
+
+  public static readonly User shipper = new User("shipper@fastship.com", "0955111533", "b45cffe084dd3d20d928bee85e7b0f21", "123123", "Khang", "Ngoc", DateTime.Now, "159 aaa bbb");
+  public static readonly Shipper shipperEmployee = new Shipper("Q9", DateTime.Now, DateTime.Now, ShippingStatus.online);
 
   public static readonly User customer = new User("customer@gmail.com", "091092233", "4297f44b13955235245b2497399d7a93", "123123", "Khang", "Ngoc", DateTime.Now, "159 aaa bbb");
 
@@ -33,15 +37,22 @@ public static class SeedData
     dbContext.CurrencyExchanges.Add(US);
   }
 
-  public static void PopulateEmployee(AppDbContext dbContext)
+  public static void PopulateUsers(AppDbContext dbContext)
   {
     
     employee.setRole(dbContext.Roles.Where(r => r.roleName == RoleEnum.EMPLOYEE.ToString()).First());
 
     customer.setRole(dbContext.Roles.Where(r => r.roleName == RoleEnum.CUSTOMER.ToString()).First());
 
+    shipper.setRole(dbContext.Roles.Where(r => r.roleName == RoleEnum.SHIPPER.ToString()).First());
     dbContext.Users.Add(employee);
     dbContext.Users.Add(customer);
+    dbContext.Users.Add(shipper);
+
+    dbContext.SaveChanges();
+
+    shipperEmployee.userId = shipper.Id;
+    dbContext.Shippers.Add(shipperEmployee);
   }
 
   public static void PopulateRoles(AppDbContext dbContext)
@@ -73,7 +84,7 @@ public static class SeedData
     }
     if (!dbContext.Users.Any())
     {
-      PopulateEmployee(dbContext);
+      PopulateUsers(dbContext);
     }
 
     dbContext.SaveChanges();
