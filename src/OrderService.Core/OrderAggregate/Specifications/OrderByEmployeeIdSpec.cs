0b1f5f563decd8748028661c86ca934a32e0612b
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-04-07 01:29:31 +0700

    add order by employee Id specification

---
+++ src/OrderService.Core/OrderAggregate/Specifications/OrderByEmployeeIdSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+
+namespace OrderService.Core.OrderAggregate.Specifications;
+public class OrderByEmployeeIdSpec : Specification<Order>
+{
+  public OrderByEmployeeIdSpec(int employeeId)
+  {
+    Query
+      .Include(o => o.chat)
+      .Where(o => o.chat.employee.Id == employeeId);
+  }
+}
