8bb38ca421fabfeb57d86a3f8d90d214701fa607
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-04-07 01:29:14 +0700

    implement get most free shipper

--- src/OrderService.Core/Interfaces/IGetMostFreeEmployeeService.cs
+++ src/OrderService.Core/Interfaces/IGetMostFreeEmployeeService.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ardalis.Result;
+﻿using Ardalis.Result;
+using OrderService.Core.ShipperAggregate;
 using OrderService.Core.UserAggregate;
 
 namespace OrderService.Core.Interfaces;
 public interface IGetMostFreeEmployeeService
 {
   public Task<Result<User>> GetMostFreeEmployee();
+  public Task<Shipper?> GetMostFreeShipper();
 }
--- src/OrderService.Core/Services/GetMostFreeEmployeeService.cs
+++ src/OrderService.Core/Services/GetMostFreeEmployeeService.cs
@@ -1,6 +1,8 @@
 ﻿
 using Ardalis.Result;
 using OrderService.Core.Interfaces;
+using OrderService.Core.ShipperAggregate;
+using OrderService.Core.ShipperAggregate.specifications;
 using OrderService.Core.UserAggregate;
 using OrderService.Core.UserAggregate.Specifications;
 using OrderService.SharedKernel.Interfaces;
@@ -9,17 +11,19 @@ namespace OrderService.Core.Services;
 public class GetMostFreeEmployeeService : IGetMostFreeEmployeeService
 {
 
-  private readonly IRepository<User> _repository;
+  private readonly IRepository<User> _userRepository;
+  private readonly IRepository<Shipper> _shipperRepository;
   
-  public GetMostFreeEmployeeService(IRepository<User> repository)
+  public GetMostFreeEmployeeService(IRepository<User> repository, IRepository<Shipper> shipperRepository)
   {
-    _repository = repository;
+    _userRepository = repository;
+    _shipperRepository = shipperRepository;
   }
   
   public async Task<Result<User>> GetMostFreeEmployee()
   {
     var spec = new UserByRoleSpec(RoleEnum.EMPLOYEE);
-    var employee = await _repository.FirstOrDefaultAsync(spec);
+    var employee = await _userRepository.FirstOrDefaultAsync(spec);
 
     if (employee == null)
     {
@@ -27,5 +31,14 @@ public class GetMostFreeEmployeeService : IGetMostFreeEmployeeService
     }
 
     return new Result<User>(employee!);
+  }
+
+
+  public async Task<Shipper?> GetMostFreeShipper()
+  {
+    var spec = new FreeShipperSpec();
+    var shipper = await _shipperRepository.FirstOrDefaultAsync(spec);
+
+    return shipper;
   }
 }
