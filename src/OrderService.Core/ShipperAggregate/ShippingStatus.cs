105f2ac459ce199d7a2397b737be869d47ee0b7b
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-04-07 01:31:13 +0700

    add busy status for shipper

--- src/OrderService.Core/ShipperAggregate/ShippingStatus.cs
+++ src/OrderService.Core/ShipperAggregate/ShippingStatus.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ardalis.SmartEnum;
+﻿using Ardalis.SmartEnum;
 
 namespace OrderService.Core.ShipperAggregate;
 public class ShippingStatus : SmartEnum<ShippingStatus>
 {
-  public readonly ShippingStatus online = new(nameof(online), 1);
-  public readonly ShippingStatus offline = new(nameof(offline), 0);
+  public static readonly ShippingStatus busy = new(nameof(busy), 2);
+  public static readonly ShippingStatus online = new(nameof(online), 1);
+  public static readonly ShippingStatus offline = new(nameof(offline), 0);
 
   public ShippingStatus(string name, int value) : base(name, value) { }
 }
