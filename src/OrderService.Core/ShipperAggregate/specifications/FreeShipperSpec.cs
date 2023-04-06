43c793a338e3666fff89d6c222d92c46f3d3c3d4
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-04-07 01:30:50 +0700

    free shiper specification

---
+++ src/OrderService.Core/ShipperAggregate/specifications/FreeShipperSpec.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.Specification;
+
+namespace OrderService.Core.ShipperAggregate.specifications;
+public class FreeShipperSpec : Specification<Shipper>, ISingleResultSpecification
+{
+  public FreeShipperSpec()
+  {
+    Query
+      .Where(s => s.shippingStatus == ShippingStatus.online);
+  }
+}
