06a5f3ec13ce52a722e70fa0789c360faab71c30
khangzxrr <khangzxrr@gmail.com>
2023-04-29 19:16:57 +0700

    add shiperTaken ordershippingStatus, rename shipperId to userId

--- src/OrderService.Core/OrderShippingAggregate/OrderShippingStatus.cs
+++ src/OrderService.Core/OrderShippingAggregate/OrderShippingStatus.cs
@@ -6,8 +6,9 @@ namespace OrderService.Core.OrderShippingAggregate;
 public class OrderShippingStatus : SmartEnum<OrderShippingStatus>
 {
   public static readonly OrderShippingStatus inWarehouse = new(nameof(inWarehouse), 0);
-  public static readonly OrderShippingStatus shipping = new(nameof(shipping), 1);
-  public static readonly OrderShippingStatus customerReceived = new(nameof(customerReceived), 2);
+  public static readonly OrderShippingStatus shipperTaken = new(nameof(shipperTaken), 1);
+  public static readonly OrderShippingStatus shipping = new(nameof(shipping), 2);
+  public static readonly OrderShippingStatus customerReceived = new(nameof(customerReceived), 3);
 
   public OrderShippingStatus(string name, int value) : base(name, value) { }
 }
--- src/OrderService.Core/ShipperAggregate/specifications/ShipperWithOrderByUserIdSpec.cs
+++ src/OrderService.Core/ShipperAggregate/specifications/ShipperWithOrderByUserIdSpec.cs
@@ -4,10 +4,10 @@ namespace OrderService.Core.ShipperAggregate.specifications;
 public class ShipperWithOrderByUserIdSpec : Specification<Shipper>, ISingleResultSpecification
 {
 
-  public ShipperWithOrderByUserIdSpec(int shipperId)
+  public ShipperWithOrderByUserIdSpec(int userId)
   {
     Query
-      .Where(s => s.userId == shipperId)
+      .Where(s => s.userId == userId)
       .Include(s => s.OrderShippings)
         .ThenInclude(os => os.order)
           .ThenInclude(o => o.user);
