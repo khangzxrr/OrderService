4db25097e00c9046cbe1098841ed75de6bf35e15
khangzxrr <khangzxrr@gmail.com>
2023-05-02 20:19:07 +0700

    add order and chat by order id spec

---
+++ src/OrderService.Core/OrderAggregate/Specifications/OrderChatByIdSpec.cs
@@ -0,0 +1,14 @@
+﻿
+using Ardalis.Specification;
+
+namespace OrderService.Core.OrderAggregate.Specifications;
+public class OrderChatByIdSpec : Specification<Order>, ISingleResultSpecification
+{
+  public OrderChatByIdSpec(int orderId)
+  {
+    Query
+      .Where(o => o.Id == orderId)
+      .Include(o => o.chat)
+        .ThenInclude(c => c.chatMessages);
+  }
+}
