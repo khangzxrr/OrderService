14e4a4b93d5938a431c6509929f7845247082ce4
khangzxrr <khangzxrr@gmail.com>
2023-05-02 12:21:14 +0700

    Not <= 0 validation

---
+++ src/OrderService.Web/Validations/NotZeroAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderService.Web.Validations;
+
+public class NotZeroAttribute : ValidationAttribute
+{
+  public override bool IsValid(object? value)
+  {
+    return (double)value! <= 0;
+  }
+}
