81d8f6e765c091e8f9ff32737cabad40d23ab9eb
khangzxrr <khangzxrr@gmail.com>
2023-05-02 12:23:22 +0700

    add Shipper paying method

---
+++ src/OrderService.Core/ShipperAggregate/enums/ShipperPayingMethod.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Common;
+using Ardalis.SmartEnum;
+
+namespace OrderService.Core.ShipperAggregate.enums;
+public class ShipperPayingMethod: SmartEnum<ShipperPayingMethod>
+{
+  public static readonly ShipperPayingMethod byCash = new(nameof(byCash), 0);
+  public static readonly ShipperPayingMethod byOnline = new(nameof(byOnline), 1);
+  
+  public ShipperPayingMethod(string name, int value) : base(name, value) { }
+}
