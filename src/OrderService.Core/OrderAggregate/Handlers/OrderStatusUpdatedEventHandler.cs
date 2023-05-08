12adf2e9fc4bcafa4a7ed60480d81d233eb0a267
khangzxrr <khangzxrr@gmail.com>
2023-05-08 20:07:03 +0700

    remove chat in notifi

--- src/OrderService.Core/OrderAggregate/Handlers/OrderStatusUpdatedEventHandler.cs
+++ src/OrderService.Core/OrderAggregate/Handlers/OrderStatusUpdatedEventHandler.cs
@@ -31,8 +31,6 @@ public class OrderStatusUpdatedEventHandler : INotificationHandler<OrderStatusUp
 
     await _emailSender.SendEmailAsync(order.user.email, "fastship@gmail.com", "[FastShip] Cập nhật trạng thái đơn hàng", $"<p>Xin chào bạn, đơn hàng #{notification.OrderId} vừa được cập nhật trạng thái mới ({notification.orderStatus}) <a href='http://localhost:3000/detailod?orderId={notification.OrderId}'>Để xem chi tiết vui lòng nhấn vào đây</a></p>");
 
-    order.chat.AddNewNotifiChatMessage($"Updated order status to {notification.orderStatus.Name} at {DateTime.Now.ToString("HH:ss dd/MM/yyyy")}");
-
     await _orderRepository.SaveChangesAsync();
   }
 }
