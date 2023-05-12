dd16907c45bcdadc8be25820f2cc007ed083cbc2
khangzxrr <khangzxrr@gmail.com>
2023-05-13 02:17:43 +0700

    add manager seed data

--- src/OrderService.Web/SeedData.cs
+++ src/OrderService.Web/SeedData.cs
@@ -10,12 +10,14 @@ namespace OrderService.Web;
 public static class SeedData
 {
 
-  public static readonly User employee = new User("employee@fastship.com", "091092211", "4297f44b13955235245b2497399d7a93", "123123", "Khang", "Ngoc", DateTime.Now, "159 aaa bbb");
+  public static readonly User employee = new User("employee@fastship.com", "091092211", "4297f44b13955235245b2497399d7a93", "123123", "Tiến", "Đoàn Văn", DateTime.Now, "157 xa lộ hà nội, quận 2, TP  Hồ Chí Minh");
 
-  public static readonly User shipper = new User("shipper@fastship.com", "0955111533", "4297f44b13955235245b2497399d7a93", "123123", "Khang", "Ngoc", DateTime.Now, "159 aaa bbb");
+  public static readonly User shipper = new User("shipper@fastship.com", "0955111533", "4297f44b13955235245b2497399d7a93", "123123", "Dũng", "Đinh Văn", DateTime.Now, "14 Nguyễn Tri Phương, TP Hồ Chí Minh");
   public static readonly Shipper shipperEmployee = new Shipper("Q9", DateTime.Now, DateTime.Now, ShippingStatus.online);
 
-  public static readonly User customer = new User("customer@gmail.com", "091092233", "4297f44b13955235245b2497399d7a93", "123123", "Khang", "Ngoc", DateTime.Now, "159 aaa bbb");
+  public static readonly User customer = new User("customer@gmail.com", "091092233", "4297f44b13955235245b2497399d7a93", "123123", "Định", "Võ Ngọc", DateTime.Now, "1426 nguyễn duy trinh, quận 9, tp hồ chí minh");
+
+  public static readonly User manager = new User("manager@fastship.com", "0988111737", "4297f44b13955235245b2497399d7a93", "123123", "Đạt", "Nguyễn Quốc", DateTime.Now, "111 Huỳnh Tấn Phát, quận 7, tp hồ chí minh");
 
   public static void Initialize(IServiceProvider serviceProvider)
   {
@@ -45,9 +47,13 @@ public static class SeedData
     customer.setRole(dbContext.Roles.Where(r => r.roleName == RoleEnum.CUSTOMER.ToString()).First());
 
     shipper.setRole(dbContext.Roles.Where(r => r.roleName == RoleEnum.SHIPPER.ToString()).First());
+
+    manager.setRole(dbContext.Roles.Where(r => r.roleName == RoleEnum.MANAGER.ToString()).First());
+
     dbContext.Users.Add(employee);
     dbContext.Users.Add(customer);
     dbContext.Users.Add(shipper);
+    dbContext.Users.Add(manager);
 
     dbContext.SaveChanges();
 
