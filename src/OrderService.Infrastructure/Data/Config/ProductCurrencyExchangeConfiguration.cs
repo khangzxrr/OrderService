094e76bcdbdfbbd812bf85446e6a40d4a48dcfb6
khangzxrr <khangzxrr@gmail.com>
2023-05-13 02:15:30 +0700

    config productCurrencyExchange

---
+++ src/OrderService.Infrastructure/Data/Config/ProductCurrencyExchangeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderService.Core.CurrencyAggregate;
+
+namespace OrderService.Infrastructure.Data.Config;
+public class ProductCurrencyExchangeConfiguration : IEntityTypeConfiguration<ProductCurrencyExchange>
+{
+  public void Configure(EntityTypeBuilder<ProductCurrencyExchange> builder)
+  {
+    builder.HasOne(c => c.product).WithOne(p => p.currencyExchange);
+    builder.HasOne(c => c.currency).WithMany();
+  }
+}
