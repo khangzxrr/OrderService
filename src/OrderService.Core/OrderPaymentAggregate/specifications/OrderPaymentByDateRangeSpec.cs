ddb876c38145351a7d78a81f46d14bfaa1c4c862
khangzxrr <khangzxrr@gmail.com>
2023-05-14 02:16:39 +0700

    add specs

---
+++ src/OrderService.Core/OrderPaymentAggregate/specifications/OrderPaymentByDateRangeSpec.cs
@@ -0,0 +1,24 @@
+﻿
+
+using Ardalis.Specification;
+
+namespace OrderService.Core.OrderPaymentAggregate.specifications;
+public class OrderPaymentByDateRangeSpec: Specification<OrderPayment>
+{
+  public OrderPaymentByDateRangeSpec(DateTime? startDate, DateTime? endDate)
+  {
+
+    if (startDate == null)
+    {
+      startDate = DateTime.MinValue;
+    }
+
+    if (endDate == null)
+    {
+      endDate = DateTime.MaxValue;
+    }
+
+    Query
+      .Where(op => op.paymentDate >= startDate && op.paymentDate <= endDate);
+  }
+}
---
+++ src/OrderService.Core/OrderPaymentAggregate/specifications/OrderPaymentFilterPaginatedSpec.cs
@@ -0,0 +1,28 @@
+﻿using Ardalis.Specification;
+
+namespace OrderService.Core.OrderPaymentAggregate.specifications;
+public class OrderPaymentFilterPaginatedSpec: Specification<OrderPayment>
+{
+  public OrderPaymentFilterPaginatedSpec(int skip, int take, DateTime? startDate, DateTime? endDate)
+  {
+    if (take == 0)
+    {
+      take = int.MaxValue;
+    }
+
+    if (startDate == null)
+    {
+      startDate = DateTime.MinValue;
+    }
+
+    if (endDate == null)
+    {
+      endDate = DateTime.MaxValue;
+    }
+
+    Query
+      .Where(op => op.paymentDate.Date >= startDate.Value.Date && op.paymentDate.Date <= endDate.Value.Date)
+      .Skip(skip)
+      .Take(take);
+  }
+}
