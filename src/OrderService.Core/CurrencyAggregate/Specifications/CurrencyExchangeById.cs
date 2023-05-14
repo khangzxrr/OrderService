fb1e0ac9af4a4ae04855cff31a94bed76d761f89
khangzxrr <khangzxrr@gmail.com>
2023-05-14 11:27:28 +0700

    add select currency by id spec

---
+++ src/OrderService.Core/CurrencyAggregate/Specifications/CurrencyExchangeById.cs
@@ -0,0 +1,10 @@
+﻿using Ardalis.Specification;
+
+namespace OrderService.Core.CurrencyAggregate.Specifications;
+public class CurrencyExchangeById: Specification<CurrencyExchange>, ISingleResultSpecification
+{
+  public CurrencyExchangeById(int id) { 
+    Query
+      .Where(c => c.Id == id);
+  }
+}
