63c383881b88e248a1188a3f83d2be3bc8c12f4d
khangzxrr <khangzxrr@gmail.com>
2023-05-15 01:20:45 +0700

    add specs

---
+++ src/OrderService.Core/OrderShippingAggregate/Specifications/OrderShippingByOrderIdSpec.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+
+namespace OrderService.Core.OrderShippingAggregate.specifications;
+public class OrderShippingByOrderIdSpec : Specification<OrderShipping>, ISingleResultSpecification
+{
+  public OrderShippingByOrderIdSpec(int orderId)
+  {
+    Query
+      .Where(os => os.orderId == orderId)
+      .Include(os => os.shipper)
+        .ThenInclude(s => s!.user);
+  }
+}
---
+++ src/OrderService.Core/OrderShippingAggregate/Specifications/OrderShippingPagingatedSpec.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Ardalis.Specification;
+
+namespace OrderService.Core.OrderShippingAggregate.Specifications;
+public class OrderShippingPagingatedSpec : Specification<OrderShipping>
+{
+  public OrderShippingPagingatedSpec(int skip, int take)
+  {
+    if (take == 0)
+    {
+      take = int.MaxValue;
+    }
+
+    Query
+      .Include(os => os.shipper)
+        .ThenInclude(s => s!.user)
+      .Skip(skip) 
+      .Take(take);
+  }
+}
