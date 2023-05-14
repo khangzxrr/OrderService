b96e0f48bdd227f1946ecf3ac7d861112e00aa9a
khangzxrr <khangzxrr@gmail.com>
2023-05-14 11:27:54 +0700

    currency record

---
+++ src/OrderService.Web/Endpoints/ManagerEndpoints/CurrencyRecord.cs
@@ -0,0 +1,11 @@
+﻿using OrderService.Core.CurrencyAggregate;
+
+namespace OrderService.Web.Endpoints.ManagerEndpoints;
+
+public record CurrencyRecord(int id, string fromCurrency, float rate)
+{
+  public static CurrencyRecord fromEntity(CurrencyExchange currencyExchange)
+  {
+    return new CurrencyRecord(currencyExchange.Id, currencyExchange.fromCurrency, currencyExchange.rate);
+  }
+}
