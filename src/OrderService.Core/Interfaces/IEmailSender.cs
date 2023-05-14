86eb8d174c0eb3d39e78f758f2d24d3988e097b0
khangzxrr <khangzxrr@gmail.com>
2023-05-14 20:16:45 +0700

    remove from parameter

--- src/OrderService.Core/Interfaces/IEmailSender.cs
+++ src/OrderService.Core/Interfaces/IEmailSender.cs
@@ -4,5 +4,5 @@ namespace OrderService.Core.Interfaces;
 
 public interface IEmailSender
 {
-  Task SendEmailAsync(string to, string from, string subject, string body);
+  Task SendEmailAsync(string to, string subject, string body);
 }
