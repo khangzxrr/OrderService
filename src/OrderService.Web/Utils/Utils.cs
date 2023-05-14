c3bea835a2cea566590fd71d90cafd65daba344f
khangzxrr <khangzxrr@gmail.com>
2023-05-14 20:04:09 +0700

    prevent boiled code by seperate page calulation

---
+++ src/OrderService.Web/Utils/Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace OrderService.Web.Utils;
+
+public class Utils
+{
+  public static int getPageCount(int totalCount, int pageSize)
+  {
+    if (pageSize == 0)
+    {
+      return totalCount > 0 ? 1 : 0;
+    }
+
+    return (int)Math.Ceiling((decimal)totalCount / pageSize);
+  }
+}
