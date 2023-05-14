04f5577724e05922813f9538ca30d91b189e7ce5
khangzxrr <khangzxrr@gmail.com>
2023-05-14 11:27:45 +0700

    private set and add setRate method

--- src/OrderService.Core/CurrencyAggregate/CurrencyExchange.cs
+++ src/OrderService.Core/CurrencyAggregate/CurrencyExchange.cs
@@ -5,12 +5,17 @@ using OrderService.SharedKernel.Interfaces;
 namespace OrderService.Core.CurrencyAggregate;
 public class CurrencyExchange : EntityBase, IAggregateRoot
 {
-  public string fromCurrency { get; set; }
-  public float rate { get; set; }
+  public string fromCurrency { get; private set; }
+  public float rate { get; private set; }
 
   public CurrencyExchange(string fromCurrency, float rate)
   {
     this.fromCurrency = Guard.Against.NullOrEmpty(fromCurrency);
     this.rate = Guard.Against.Negative(rate);
+  }
+
+  public void setRate(float rate)
+  {
+    this.rate = Guard.Against.NegativeOrZero(rate);
   }
 }
