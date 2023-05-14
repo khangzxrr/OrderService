3c8bc18d8002cab9e9f320ffa39ef59d30e8a9cf
khangzxrr <khangzxrr@gmail.com>
2023-05-14 20:03:02 +0700

    new specs

---
+++ src/OrderService.Core/OrderAggregate/Specifications/GeneralOrderPaginated.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.Specification;
+
+namespace OrderService.Core.OrderAggregate.Specifications;
+public class GeneralOrderPaginated: Specification<Order>
+{
+  public GeneralOrderPaginated(int skip, int take, DateTime? startDate, DateTime? endDate)
+  {
+
+    if (take == 0)
+    {
+      take = int.MaxValue;
+    }
+
+    if (startDate == null)
+    {
+      startDate = DateTime.MinValue;
+    }
+
+    if (endDate == null)
+    {
+      endDate = DateTime.MaxValue;
+    }
+
+
+    Query
+      .Include(o => o.user)
+      .Where(o => o.orderDate.Date >= startDate.Value.Date && o.orderDate.Date <= endDate.Value.Date)
+      .Skip(skip)
+      .Take(take);
+  }
+}
---
+++ src/OrderService.Core/OrderAggregate/Specifications/GeneralOrderPaginatedByStatusAndDateSpec.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.Specification;
+
+namespace OrderService.Core.OrderAggregate.Specifications;
+public class GeneralOrderPaginatedByStatusAndDateSpec: Specification<Order>
+{
+  public GeneralOrderPaginatedByStatusAndDateSpec(int skip, int take, DateTime? startDate, DateTime? endDate, OrderStatus status)
+  {
+
+    if (take == 0)
+    {
+      take = int.MaxValue;
+    }
+
+    if (startDate == null)
+    {
+      startDate = DateTime.MinValue;
+    }
+
+    if (endDate == null)
+    {
+      endDate = DateTime.MaxValue;
+    }
+
+
+    Query
+      .Include(o => o.user)
+      .Where(o => o.orderDate.Date >= startDate.Value.Date && o.orderDate.Date <= endDate.Value.Date && o.status == status)
+      .Skip(skip)
+      .Take(take);
+  }
+}
