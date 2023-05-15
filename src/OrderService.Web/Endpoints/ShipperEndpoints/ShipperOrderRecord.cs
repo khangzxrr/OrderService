0c9533358e25338069b28eb7076e176c43312e71
khangzxrr <khangzxrr@gmail.com>
2023-05-15 14:47:41 +0700

    add orderId to shipperOrderRecord

--- src/OrderService.Web/Endpoints/ShipperEndpoints/ShipperOrderRecord.cs
+++ src/OrderService.Web/Endpoints/ShipperEndpoints/ShipperOrderRecord.cs
@@ -3,11 +3,11 @@ using OrderService.Core.OrderShippingAggregate;
 
 namespace OrderService.Web.Endpoints.ShipperEndpoints;
 
-public record ShipperOrderRecord(int orderShippingId, string customerName, string customerAddress, string customerPhoneNumber, string orderShipingStatus, double remainCost)
+public record ShipperOrderRecord(int orderShippingId, string customerName, string customerAddress, string customerPhoneNumber, string orderShipingStatus, double remainCost, int orderId)
 {
 
   public static ShipperOrderRecord FromEntity(OrderShipping orderShipping)
   {
-    return new ShipperOrderRecord(orderShipping.Id, orderShipping.order.user.firstname, orderShipping.order.user.address, orderShipping.order.user.phoneNumber, orderShipping.orderShippingStatus.Name, orderShipping.order.remainCost);
+    return new ShipperOrderRecord(orderShipping.Id, orderShipping.order.user.firstname, orderShipping.order.user.address, orderShipping.order.user.phoneNumber, orderShipping.orderShippingStatus.Name, orderShipping.order.remainCost, orderShipping.orderId);
   }
 }
