599e8359d4ce5a93b8a57f68188a7b3a920f826b
khangzxrr <khangzxrr@gmail.com>
2023-05-15 14:48:36 +0700

    add costperweight migration

---
+++ src/OrderService.Infrastructure/Migrations/20230515053954_AddCostPerWeightToOrderDetail.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCostPerWeightToOrderDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "costPerWeight",
+                table: "OrderDetail",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "costPerWeight",
+                table: "OrderDetail");
+        }
+    }
+}
