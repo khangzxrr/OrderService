728a8f8615728721b81a94623256bdb5571f383b
khangzxrr <khangzxrr@gmail.com>
2023-05-15 14:45:02 +0700

    add order detail update event handler

---
+++ src/OrderService.Core/OrderAggregate/Events/OrderDetailUpdatedWeightEvent.cs
@@ -0,0 +1,15 @@
+﻿using OrderService.Core.ProductAggregate;
+using OrderService.SharedKernel;
+
+namespace OrderService.Core.OrderAggregate.Events;
+public class OrderDetailUpdatedWeightEvent : DomainEventBase
+{
+  public Order order { get; set; }
+  public Product product { get; set; }
+
+  public OrderDetailUpdatedWeightEvent(Order order, Product product)
+  {
+    this.order = order;
+    this.product = product;
+  }
+}
---
+++ src/OrderService.Core/OrderAggregate/Handlers/OrderDetailUpdatedWeightHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using OrderService.Core.Interfaces;
+using OrderService.Core.OrderAggregate.Events;
+namespace OrderService.Core.OrderAggregate.Handlers;
+public class OrderDetailUpdatedWeightHandler : INotificationHandler<OrderDetailUpdatedWeightEvent>
+{
+
+  private readonly IEmailSender _emailSender;
+
+  public OrderDetailUpdatedWeightHandler(IEmailSender emailSender)
+  {
+    _emailSender = emailSender;
+  }
+
+  public async Task Handle(OrderDetailUpdatedWeightEvent notification, CancellationToken cancellationToken)
+  {
+    await _emailSender.SendEmailAsync(notification.order.user.email, "[FastShip] Cập nhật trọng lượng sản phẩm", $"<p>Xin chào bạn, đơn hàng #{notification.order.Id} đã được cập nhật trọng lượng sản phẩm {notification.product.productName} thành {notification.product.productWeight}kg vì vậy tổng giá trị đơn hàng đã được thay đổi thành {notification.order.price} <a href='http://localhost:3000/detailod?orderId={notification.order.Id}'>Để xem chi tiết vui lòng nhấn vào đây</a></p>");
+  }
+}
