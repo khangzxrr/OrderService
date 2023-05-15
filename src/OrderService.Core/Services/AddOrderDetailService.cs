0bb9bb0c0435971b6b331f201138327ede3006e8
khangzxrr <khangzxrr@gmail.com>
2023-05-15 14:46:30 +0700

    implement updateorderdetails and seperate rule engine

--- src/OrderService.Core/Services/AddOrderDetailService.cs
+++ src/OrderService.Core/Services/AddOrderDetailService.cs
@@ -19,6 +19,64 @@ public class AddOrderDetailService : IAddOrderDetailService
     _productRepository = productRepository;
   }
 
+  public async Task<Result> UpdateOrderDetails(Order order)
+  {
+
+    double totalOrderCost = 0.0f;
+
+    foreach(var orderDetail in order.orderDetails)
+    {
+      var additionalCostRuleEngine = await runRuleEngine(orderDetail);
+
+      var additionalCostWithWeightCost = additionalCostRuleEngine  + (orderDetail.product.productWeight * orderDetail.costPerWeight);
+
+      orderDetail.setAdditionalCost(additionalCostWithWeightCost);
+
+      var totalOrderDetailCost = orderDetail.shipCost;
+      totalOrderDetailCost += orderDetail.product.productPrice * orderDetail.quantity;
+      totalOrderDetailCost += orderDetail.additionalCost;
+      totalOrderDetailCost += orderDetail.processCost;
+
+      orderDetail.setTotalCost(totalOrderDetailCost);
+
+      totalOrderCost += totalOrderDetailCost * orderDetail.product.currencyExchange.rate;
+    }
+
+    totalOrderCost = Math.Ceiling(totalOrderCost);
+
+    //IMPORTANT
+    var newRemainCost = order.remainCost + (totalOrderCost - order.price);
+
+    order.SetPrice(totalOrderCost);
+    order.SetRemainCost(newRemainCost);
+
+    return Result.Success();
+  }
+
+  private async Task<float> runRuleEngine(OrderDetail orderDetail)
+  {
+    var conditionString = orderDetail.product.productCategory.productShipCost.additionalCostCondition.Replace('\t', ' ');
+    var workflow = JsonConvert.DeserializeObject<List<Workflow>>(conditionString);
+
+    var re = new RulesEngine.RulesEngine(workflow!.ToArray(), null);
+
+
+    var rule1 = new RuleParameter("orderDetail", new
+    {
+      productCost = orderDetail.product.productPrice
+    });
+    List<RuleParameter> ruleParameters = new List<RuleParameter>
+    {
+      rule1
+    };
+
+    var result = await re.ExecuteActionWorkflowAsync("AdditionalCost", "Price_over", ruleParameters.ToArray());
+
+    float additionalCost = (float)Convert.ToDouble(result.Output);
+
+    return additionalCost;
+  }
+
   public async Task<Result> AddOrderDetail(Order order, int productId, int quantity)
   {
     if (order == null)
@@ -42,25 +100,8 @@ public class AddOrderDetailService : IAddOrderDetailService
     orderDetail.setProduct(product);
     orderDetail.setQuantity(quantity);
 
+    float additionalCost = await runRuleEngine(orderDetail);
 
-    var conditionString = product.productCategory.productShipCost.additionalCostCondition.Replace('\t',' ');
-    var workflow = JsonConvert.DeserializeObject<List<Workflow>>(conditionString);
-
-    var re = new RulesEngine.RulesEngine(workflow!.ToArray(), null);
-
-
-    var rule1 = new RuleParameter("orderDetail", new
-    {
-      productCost = orderDetail.product.productPrice
-    });
-    List<RuleParameter> ruleParameters = new List<RuleParameter>
-    {
-      rule1 
-    };
-
-    var result = await re.ExecuteActionWorkflowAsync("AdditionalCost", "Price_over", ruleParameters.ToArray());
-
-    float additionalCost = (float)Convert.ToDouble(result.Output);
     orderDetail.setAdditionalCost(additionalCost);
     order.addOrderDetail(orderDetail);
 
