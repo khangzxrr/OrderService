dd73abf30ec002b0c658296a10be77cf0351193e
khangzxrr <khangzxrr@gmail.com>
2023-05-15 14:44:42 +0700

    add updateorderdetails method

--- src/OrderService.Core/Interfaces/IAddOrderDetailService.cs
+++ src/OrderService.Core/Interfaces/IAddOrderDetailService.cs
@@ -1,9 +1,9 @@
 ﻿using Ardalis.Result;
 using OrderService.Core.OrderAggregate;
-using OrderService.Core.ProductAggregate;
 
 namespace OrderService.Core.Interfaces;
 public interface IAddOrderDetailService
 {
   public Task<Result> AddOrderDetail(Order order, int productId, int quantity);
+  public Task<Result> UpdateOrderDetails(Order order);
 }
