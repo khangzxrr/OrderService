6cb50a0b5f20e878b510a6f0c0f0e834d890a31d
khangzxrr <khangzxrr@gmail.com>
2023-05-20 01:55:09 +0700

    add base paginated request/response

---
+++ src/OrderService.Web/Endpoints/BaseEndpoints/BasePaginatedRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderService.Web.Endpoints.BaseEndpoints;
+
+public abstract class BasePaginatedRequest
+{
+  [Required]
+  public int pageIndex { get; set; }
+  [Required]
+  public int pageSize { get; set; } = 10;
+
+  public int skip => pageIndex * pageSize;
+  public int take => (pageSize == 0) ? int.MaxValue : pageSize;
+
+
+}
---
+++ src/OrderService.Web/Endpoints/BaseEndpoints/BasePaginatedResponse.cs
@@ -0,0 +1,17 @@
+﻿using OrderService.Core.ShipperAggregate;
+
+namespace OrderService.Web.Endpoints.BaseEndpoints;
+
+public abstract class BasePaginatedResponse<T>
+{
+
+  public int pageCount { get; set; }
+  public IEnumerable<T> records { get; set; }
+
+  public BasePaginatedResponse(int totalCount, int pageSize, IEnumerable<T> records)
+  {
+    this.pageCount = Utils.Utils.getPageCount(totalCount, pageSize);
+    this.records = records;
+  }
+
+}
