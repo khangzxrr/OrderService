748a3033dfc2a067da34507b6420721b8fde9134
khangzxrr <khangzxrr@gmail.com>
2023-05-21 00:10:05 +0700

    add noPriceQoutation status

--- src/OrderService.Core/OrderAggregate/OrderStatus.cs
+++ src/OrderService.Core/OrderAggregate/OrderStatus.cs
@@ -3,13 +3,15 @@
 namespace OrderService.Core.OrderAggregate;
 public class OrderStatus : SmartEnum<OrderStatus>
 {
-  public static readonly OrderStatus noPayYet = new(nameof(noPayYet), 0);
-  public static readonly OrderStatus waitingToOrderFromSeller = new(nameof(waitingToOrderFromSeller), 1);
-  public static readonly OrderStatus orderingFromSeller = new(nameof(orderingFromSeller), 2);
-  public static readonly OrderStatus deliverFromUsToVN = new(nameof(deliverFromUsToVN), 3);
-  public static readonly OrderStatus inVNwarehouse = new(nameof(inVNwarehouse), 4);
-  public static readonly OrderStatus deliverToCustomer = new(nameof(deliverToCustomer), 5);
-  public static readonly OrderStatus finished = new(nameof(finished), 6);
+  public static readonly OrderStatus noPriceQuotation = new(nameof(noPriceQuotation), 0);
+
+  public static readonly OrderStatus noPayYet = new(nameof(noPayYet), 1);
+  public static readonly OrderStatus waitingToOrderFromSeller = new(nameof(waitingToOrderFromSeller), 2);
+  public static readonly OrderStatus orderingFromSeller = new(nameof(orderingFromSeller), 3);
+  public static readonly OrderStatus deliverFromUsToVN = new(nameof(deliverFromUsToVN), 4);
+  public static readonly OrderStatus inVNwarehouse = new(nameof(inVNwarehouse), 5);
+  public static readonly OrderStatus deliverToCustomer = new(nameof(deliverToCustomer), 6);
+  public static readonly OrderStatus finished = new(nameof(finished), 7);
 
   public OrderStatus(string name, int value): base(name, value) { }
 }
