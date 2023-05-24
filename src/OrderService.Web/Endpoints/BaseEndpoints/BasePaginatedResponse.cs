aeaa44da8949ca1343ac453ce1c609c28371ccc7
khangzxrr <khangzxrr@gmail.com>
2023-05-24 12:18:59 +0700

    paginated now return total count instead of page count

--- src/OrderService.Web/Endpoints/BaseEndpoints/BasePaginatedResponse.cs
+++ src/OrderService.Web/Endpoints/BaseEndpoints/BasePaginatedResponse.cs
@@ -5,12 +5,12 @@ namespace OrderService.Web.Endpoints.BaseEndpoints;
 public abstract class BasePaginatedResponse<T>
 {
 
-  public int pageCount { get; set; }
+  public int totalCount { get; set; }
   public IEnumerable<T> records { get; set; }
 
   public BasePaginatedResponse(int totalCount, int pageSize, IEnumerable<T> records)
   {
-    this.pageCount = Utils.Utils.getPageCount(totalCount, pageSize);
+    this.totalCount = totalCount;
     this.records = records;
   }
 
