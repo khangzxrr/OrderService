33df994f6217b6ce0f211c3665880f1d359d3ef3
khangzxrr <khangzxrr@gmail.com>
2023-05-24 18:53:11 +0700

    add new spec

---
+++ src/OrderService.Core/OrderAggregate/Specifications/OrderWithPaymentsByUserIdSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+
+namespace OrderService.Core.OrderAggregate.Specifications;
+public class OrderWithPaymentsByUserIdSpec: Specification<Order>
+{
+  public OrderWithPaymentsByUserIdSpec(int userId)
+  {
+    Query
+      .Include(o => o.orderPayments)
+      .Where(o => o.userId == userId);
+  }
+}
