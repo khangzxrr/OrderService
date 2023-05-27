85fea91d81fa5f01dbabd4f873ac850542ff80d3
khangzxrr <khangzxrr@gmail.com>
2023-05-28 03:56:55 +0700

    remove OD create event => OD update event, add handler to calculate price

---
+++ src/OrderService.Core/OrderAggregate/Events/OrderDetailUpdateEvent.cs
@@ -0,0 +1,12 @@
+﻿using OrderService.SharedKernel;
+
+namespace OrderService.Core.OrderAggregate.Events;
+public class OrderDetailUpdateEvent : DomainEventBase
+{
+  public Order order{ get; } 
+
+  public OrderDetailUpdateEvent(Order order)
+  {
+    this.order = order;
+  }
+}
---
+++ src/OrderService.Core/OrderAggregate/Handlers/OrderDetailUpdateEventHandler.cs
@@ -0,0 +1,48 @@
+﻿using Hangfire;
+using MediatR;
+using OrderService.Core.Interfaces;
+using OrderService.Core.OrderAggregate.Events;
+using OrderService.Core.OrderAggregate.Specifications;
+using OrderService.SharedKernel.Interfaces;
+
+namespace OrderService.Core.OrderAggregate.Handlers;
+public class OrderDetailUpdateEventHandler : INotificationHandler<OrderDetailUpdateEvent>
+{
+  private readonly IRepository<Order> _orderRepository;
+  private readonly IEmailSender _emailSender;
+
+  public OrderDetailUpdateEventHandler(IRepository<Order> orderRepository, IEmailSender emailSender)
+  {
+    _orderRepository = orderRepository;
+    _emailSender = emailSender;
+  }
+
+  public async Task Handle(OrderDetailUpdateEvent notification, CancellationToken cancellationToken)
+  {
+    var order = notification.order;
+
+    double totalCost = 0.0f;
+
+    float totalCostOfOrderDetail;
+
+    foreach (var orderDetail in order.orderDetails)
+    {
+      totalCostOfOrderDetail = orderDetail.shipCost;
+      totalCostOfOrderDetail += orderDetail.product.productPrice * orderDetail.quantity;
+      totalCostOfOrderDetail += orderDetail.additionalCost;
+      totalCostOfOrderDetail += orderDetail.processCost;
+
+      orderDetail.setTotalCost(totalCostOfOrderDetail);
+
+      totalCost += totalCostOfOrderDetail * orderDetail.product.currencyExchange.rate;
+    }
+
+    totalCost = Math.Ceiling(totalCost);
+
+    order.SetPrice(totalCost);
+
+    await _orderRepository.SaveChangesAsync();
+
+    _emailSender.SendEmail(order.user.email, "[FastShip] Cập nhật mới!", $"<p>Đơn hàng của bạn có cập nhật mới!  <a href='http://localhost:3000/detailod?orderId={order.Id}'>Để xem chi tiết vui lòng nhấn vào đây</a></p>");
+  }
+}
