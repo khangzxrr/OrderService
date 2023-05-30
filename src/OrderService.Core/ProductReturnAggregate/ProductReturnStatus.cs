c58e518772b8a6bb92862f7999b3338a8a8b660d
khangzxrr <khangzxrr@gmail.com>
2023-05-31 00:38:07 +0700

    add new states

--- src/OrderService.Core/ProductReturnAggregate/ProductReturnStatus.cs
+++ src/OrderService.Core/ProductReturnAggregate/ProductReturnStatus.cs
@@ -3,11 +3,15 @@
 namespace OrderService.Core.ProductReturnAggregate;
 public class ProductReturnStatus : SmartEnum<ProductReturnStatus>
 {
-  public static ProductReturnStatus verifying = new(nameof(verifying), 0);
+  public static ProductReturnStatus requestReturn = new(nameof(requestReturn), 0);
+  public static ProductReturnStatus requestWarranty = new(nameof(requestWarranty), 1);
 
-  public static ProductReturnStatus acceptReturnEmployeeFault = new(nameof(acceptReturnEmployeeFault), 1);
-  public static ProductReturnStatus acceptReturnCustomerFault = new(nameof(acceptReturnEmployeeFault), 2);
-  public static ProductReturnStatus acceptReturnSellerFault = new(nameof(acceptReturnSellerFault), 3);
+
+  public static ProductReturnStatus acceptReturnEmployeeFault = new(nameof(acceptReturnEmployeeFault), 2);
+  public static ProductReturnStatus acceptReturnCustomerFault = new(nameof(acceptReturnEmployeeFault), 3);
+  public static ProductReturnStatus acceptReturnSellerFault = new(nameof(acceptReturnSellerFault), 4);
+
+  public static ProductReturnStatus acceptWarranty = new(nameof(acceptWarranty), 5);
 
 
 
