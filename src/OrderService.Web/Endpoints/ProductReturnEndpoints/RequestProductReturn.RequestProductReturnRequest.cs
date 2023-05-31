bd3d40370312613658390f423e8773059d941b62
khangzxrr <khangzxrr@gmail.com>
2023-06-01 01:22:29 +0700

    add request product return endpoint

---
+++ src/OrderService.Web/Endpoints/ProductReturnEndpoints/RequestProductReturn.RequestProductReturnRequest.cs
@@ -0,0 +1,28 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Web.Endpoints.ProductReturnEndpoints;
+
+public class RequestProductReturnRequest
+{
+  public const string Route = "/productReturn/request";
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+
+  [Required]
+  public string[] medias { get; set; }
+
+  [Required]
+  public bool isWarranty { get; set; }
+
+  [Required]
+  public int orderDetailId { get; set; }
+
+  public string? series { get; set; }
+  public string? description { get; set; }
+
+
+
+}
---
+++ src/OrderService.Web/Endpoints/ProductReturnEndpoints/RequestProductReturn.RequestProductReturnResponse.cs
@@ -0,0 +1,13 @@
+﻿using OrderService.Web.Endpoints.Records;
+
+namespace OrderService.Web.Endpoints.ProductReturnEndpoints;
+
+public class RequestProductReturnResponse
+{
+  public ProductReturnRecord productReturnRecord { get; set; }
+
+  public RequestProductReturnResponse(ProductReturnRecord productReturnRecord)
+  {
+    this.productReturnRecord = productReturnRecord;
+  }
+}
