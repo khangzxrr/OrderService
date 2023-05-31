3989ae3a667869d5e07ead028caea9c5b78e65ab
khangzxrr <khangzxrr@gmail.com>
2023-06-01 01:20:13 +0700

    new spec

---
+++ src/OrderService.Core/OrderAggregate/Specifications/OrderByOrderDetailIdSpec.cs
@@ -0,0 +1,22 @@
+﻿
+using Ardalis.Specification;
+
+namespace OrderService.Core.OrderAggregate.Specifications;
+public class OrderByOrderDetailIdSpec : Specification<Order>
+{
+  public OrderByOrderDetailIdSpec(int orderDetailId)
+  {
+    Query
+      .Include(o => o.orderDetails.Where(od => od.Id == orderDetailId))
+        .ThenInclude(od => od.product)
+          .ThenInclude(p => p.productCategory)
+      .Include(o => o.orderDetails.Where(od => od.Id == orderDetailId))
+        .ThenInclude(od => od.product)
+          .ThenInclude(p => p.currencyExchange)
+            .ThenInclude(p => p.currency);
+
+
+
+
+  }
+}
