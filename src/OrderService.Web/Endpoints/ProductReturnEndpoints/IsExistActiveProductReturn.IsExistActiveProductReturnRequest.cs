0d1a98393f513367e85e33f084e44f33b5624837
khangzxrr <khangzxrr@gmail.com>
2023-06-01 01:22:05 +0700

    add check exist active product return enpoint

---
+++ src/OrderService.Web/Endpoints/ProductReturnEndpoints/IsExistActiveProductReturn.IsExistActiveProductReturnRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderService.Web.Endpoints.ProductReturnEndpoints;
+
+public class IsExistActiveProductReturnRequest
+{
+  public const string Route = "/productReturn/isExistActive";
+
+  [Required]
+  public int productId { get; set; }
+}
---
+++ src/OrderService.Web/Endpoints/ProductReturnEndpoints/IsExistActiveProductReturn.IsExistActiveProductReturnResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderService.Web.Endpoints.ProductReturnEndpoints;
+
+public class IsExistActiveProductReturnResponse
+{
+  public bool isExist { get; set; }
+  public int productReturnId { get; set; }
+
+  public IsExistActiveProductReturnResponse(bool isExist, int productReturnId)
+  {
+    this.isExist = isExist;
+    this.productReturnId = productReturnId;
+  }
+}
