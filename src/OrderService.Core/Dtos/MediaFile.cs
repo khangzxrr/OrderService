57338ca8295cef1b1d938f97f8cb4b6f3c024718
khangzxrr <khangzxrr@gmail.com>
2023-06-01 01:20:05 +0700

    add minio upload download service

---
+++ src/OrderService.Core/Dtos/MediaFile.cs
@@ -0,0 +1,4 @@
+﻿namespace OrderService.Core.Dtos;
+public record MediaFile(string fileName, Stream fileStream, long fileSize)
+{
+}
---
+++ src/OrderService.Core/Interfaces/IMediaService.cs
@@ -0,0 +1,10 @@
+﻿
+using OrderService.Core.Dtos;
+
+namespace OrderService.Core.Interfaces;
+public interface IMediaService
+{
+  Task<string[]> uploadFiles(MediaFile[] files);
+  Task<string> uploadFile(MediaFile file);
+  Task<Stream> getFile(string fileName);
+}
