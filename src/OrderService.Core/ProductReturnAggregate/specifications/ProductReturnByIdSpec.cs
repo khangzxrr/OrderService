6ee81f390ccc211f9248b3a5db72bb45d0556f86
khangzxrr <khangzxrr@gmail.com>
2023-06-01 01:20:44 +0700

    new specs

---
+++ src/OrderService.Core/ProductReturnAggregate/specifications/ProductReturnByIdSpec.cs
@@ -0,0 +1,19 @@
+﻿
+using Ardalis.Specification;
+
+namespace OrderService.Core.ProductReturnAggregate.specifications;
+public class ProductReturnByIdSpec: Specification<ProductReturn>, ISingleResultSpecification
+{
+  public ProductReturnByIdSpec(int id)
+  {
+    Query
+      .Include(pr => pr.product)
+        .ThenInclude(p => p.productCategory)
+      .Include(pr => pr.product)
+        .ThenInclude(p => p.currencyExchange)
+          .ThenInclude(ce => ce.currency)
+      .Include(pr => pr.ReturnMedias)
+
+      .Where(pr => pr.Id == id);
+  }
+}
---
+++ src/OrderService.Core/ProductReturnAggregate/specifications/ProductReturnByProductIdSpec.cs
@@ -0,0 +1,15 @@
+﻿
+using Ardalis.Specification;
+
+namespace OrderService.Core.ProductReturnAggregate.specifications;
+public class ProductReturnByProductIdSpec : Specification<ProductReturn>, ISingleResultSpecification
+{
+  public ProductReturnByProductIdSpec(int productId)
+  {
+    Query
+      .Include(pr => pr.product)
+      .Where(pr => pr.product.Id == productId)
+      .Where(pr => pr.finishStatus == ProductReturnFinishStatus.onGoing);
+
+  }
+}
