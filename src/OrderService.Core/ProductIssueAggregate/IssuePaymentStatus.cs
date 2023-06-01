b3326bfdef8ced7d4a5ec89221707cf1502c2282
khangzxrr <khangzxrr@gmail.com>
2023-06-02 00:22:54 +0700

    simplify state

--- src/OrderService.Core/ProductIssueAggregate/IssuePaymentStatus.cs
+++ src/OrderService.Core/ProductIssueAggregate/IssuePaymentStatus.cs
@@ -7,9 +7,8 @@ public class IssuePaymentStatus : SmartEnum<IssuePaymentStatus>
 
   public static IssuePaymentStatus customerPay = new(nameof(customerPay), 0);
 
-  public static IssuePaymentStatus refund_100 = new(nameof(refund_100), 1);
+  public static IssuePaymentStatus refund = new(nameof(refund), 1);
 
-  public static IssuePaymentStatus refund_50 = new(nameof(refund_50), 2);
 
 
   public IssuePaymentStatus(string name, int value) : base(name, value)
