5fc7fc1a1f0da972eac3dafe4b48f98e1a16c47a
khangzxrr <khangzxrr@gmail.com>
2023-06-02 00:21:48 +0700

    add refund configuration

---
+++ src/OrderService.Infrastructure/Data/Config/ProductIssueRefundConfigurationConfiguration.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderService.Core.ProductIssueAggregate;
+using OrderService.Core.ProductReturnAggregate;
+
+namespace OrderService.Infrastructure.Data.Config;
+public class ProductIssueRefundConfigurationConfiguration : IEntityTypeConfiguration<ProductIssueRefundConfiguration>
+{
+  public void Configure(EntityTypeBuilder<ProductIssueRefundConfiguration> builder)
+  {
+    builder.Property(p => p.productIssueStatus)
+      .HasConversion(
+        p => p.Value,
+        p => ProductIssueStatus.FromValue(p)
+      ).IsRequired().HasDefaultValue(ProductIssueStatus.request);
+  }
+}
