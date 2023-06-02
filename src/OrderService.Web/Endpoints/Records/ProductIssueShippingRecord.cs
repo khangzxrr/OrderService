279b8f06a98d77a2fd0a8e56bf71b8d310f89ce6
khangzxrr <khangzxrr@gmail.com>
2023-06-02 14:39:26 +0700

    new records

---
+++ src/OrderService.Web/Endpoints/Records/ProductIssueShippingRecord.cs
@@ -0,0 +1,14 @@
+﻿using OrderService.Core.ProductReturnAggregate;
+
+namespace OrderService.Web.Endpoints.Records;
+
+public record ProductIssueShippingRecord(int id, string productName, string customerFullname, string customerPhonenumber, string customerAddress, string shippingStatus , float customerPayAmount)
+{
+  public static ProductIssueShippingRecord FromEntity(ProductIssue productIssue)
+  {
+
+    float customerPayAmount = productIssue.issuePayments.Where(p => p.paymentStatus == IssuePaymentStatus.customerPay).Sum(p => p.cost);
+
+    return new ProductIssueShippingRecord(productIssue.Id, productIssue.product.productName, productIssue.customerFullname, productIssue.customerPhonenumber, productIssue.customerAddress, productIssue.productIssueShipping!.shippingStatus.Name, customerPayAmount);
+  }
+}
---
+++ src/OrderService.Web/Endpoints/Records/ProductIssueStateTrackingRecord.cs
@@ -0,0 +1,11 @@
+﻿using OrderService.Core.ProductIssueAggregate;
+
+namespace OrderService.Web.Endpoints.Records;
+
+public record ProductIssueStateTrackingRecord(int id, string status, DateTime changeDate)
+{
+  public static ProductIssueStateTrackingRecord FromEntity(IssueStateTracking issueStateTracking)
+  {
+    return new ProductIssueStateTrackingRecord(issueStateTracking.Id, issueStateTracking.productIssueStatus.Name, issueStateTracking.changeDate);
+  }
+}
