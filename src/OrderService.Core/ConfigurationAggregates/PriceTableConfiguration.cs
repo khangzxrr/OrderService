5eb31ff0921e0ecd9db007d46741dca133801ace
khangzxrr <khangzxrr@gmail.com>
2023-06-03 05:36:41 +0700

    add price table

---
+++ src/OrderService.Core/ConfigurationAggregates/PriceTableConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using OrderService.SharedKernel;
+using OrderService.SharedKernel.Interfaces;
+
+namespace OrderService.Core.ConfigurationAggregates;
+public class PriceTableConfiguration : EntityBase, IAggregateRoot
+{
+  public string categoryName { get; set; }
+  public string priceWeightDescription { get; set; }
+  public string additionalCostDescription { get; set; }
+
+  public PriceTableConfiguration(string categoryName, string priceWeightDescription, string additionalCostDescription)
+  {
+    this.categoryName = categoryName;
+    this.priceWeightDescription = priceWeightDescription;
+    this.additionalCostDescription = additionalCostDescription;
+  }
+}
