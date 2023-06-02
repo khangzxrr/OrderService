560ab6a37797e6c17cd722b34e1dc8d0c510f856
khangzxrr <khangzxrr@gmail.com>
2023-06-02 14:37:18 +0700

    move to seperate aggregate

---
+++ src/OrderService.Core/ProductIssueRefundConfiguration/ProductIssueRefundConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using OrderService.Core.ProductReturnAggregate;
+using OrderService.SharedKernel;
+using OrderService.SharedKernel.Interfaces;
+
+namespace OrderService.Core.ProductIssueRefundConfiguration;
+public class ProductIssueRefundConfiguration : EntityBase, IAggregateRoot
+{
+  public ProductIssueStatus productIssueStatus { get; private set; }
+  public float refundRate { get; private set; }
+
+  public ProductIssueRefundConfiguration(ProductIssueStatus productIssueStatus, float refundRate)
+  {
+    this.productIssueStatus = productIssueStatus;
+    this.refundRate = refundRate;
+  }
+
+
+}
