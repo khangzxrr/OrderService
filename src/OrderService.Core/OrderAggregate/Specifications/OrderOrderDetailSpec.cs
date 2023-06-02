931410b67d68a869b0f411c8eefc874300d81c70
khangzxrr <khangzxrr@gmail.com>
2023-06-02 14:33:41 +0700

    rename spec

---
+++ src/OrderService.Core/OrderAggregate/Specifications/OrderOrderDetailSpec.cs
@@ -0,0 +1,27 @@
+﻿
+using Ardalis.Specification;
+
+namespace OrderService.Core.OrderAggregate.Specifications;
+public class OrderOrderDetailSpec : Specification<Order>
+{
+  public OrderOrderDetailSpec()
+  {
+    Query
+      .Include(o => o.orderDetails)
+        .ThenInclude(od => od.product)
+            .ThenInclude(p => p.productCategory)
+      .Include(o => o.orderDetails)
+         .ThenInclude(od => od.product)
+            .ThenInclude(p => p.currencyExchange)
+              .ThenInclude(p => p.currency)
+
+      .Include(o => o.chat)
+        .ThenInclude(c => c.employee)
+      .Include(o => o.user);
+      
+
+
+
+
+  }
+}
