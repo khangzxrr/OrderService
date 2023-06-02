0acd39109b769479d731f23a6f70a35fff337664
khangzxrr <khangzxrr@gmail.com>
2023-06-03 05:37:03 +0700

    Update order detail sync

--- src/OrderService.Core/Interfaces/IAddOrderDetailService.cs
+++ src/OrderService.Core/Interfaces/IAddOrderDetailService.cs
@@ -5,5 +5,5 @@ namespace OrderService.Core.Interfaces;
 public interface IAddOrderDetailService
 {
   public Task<Result> AddOrderDetail(Order order, int productId, int quantity);
-  public Task<Result> UpdateOrderDetails(Order order);
+  public Result UpdateOrderDetails(Order order);
 }
