4d9f7c832cf9ba335be6eaa84b45443ce9c90e60
khangzxrr <khangzxrr@gmail.com>
2023-06-02 14:39:17 +0700

    rename

--- src/OrderService.Web/Endpoints/Records/ProductIssueRecord.cs
+++ src/OrderService.Web/Endpoints/Records/ProductIssueRecord.cs
@@ -2,23 +2,24 @@
 
 namespace OrderService.Web.Endpoints.Records;
 
-public record ProductIssueRecord( ProductRecord productRecord, int id, string status, int statusCode, IEnumerable<string> medias, DateTime returnDate, string returnReason, bool isWarranty, string customerEmail, string customerFullname, string customerPhonenumber, string series)
+public record ProductIssueRecord( ProductRecord productRecord, int id, string status, int statusCode, IEnumerable<string> medias, DateTime returnDate, string returnReason, bool isWarranty, string customerEmail, string customerFullname, string customerPhonenumber, string series, IEnumerable<ProductIssueStateTrackingRecord> stateTracking)
 {
-  public static ProductIssueRecord FromEntity(ProductIssue productReturn)
+  public static ProductIssueRecord FromEntity(ProductIssue productIssue)
   {
     return new ProductIssueRecord(
-      ProductRecord.FromEntity(productReturn.product),
-      productReturn.Id,
-      productReturn.status.Name,
-      productReturn.status.Value,
-      productReturn.issueMedias.Select(m => m.mediaUrl),
-      productReturn.returnDate,
-      productReturn.returnReason,
-      productReturn.isWarranty,
-      productReturn.customerEmail,
-      productReturn.customerFullname,
-      productReturn.customerPhonenumber,
-      productReturn.series
+      ProductRecord.FromEntity(productIssue.product),
+      productIssue.Id,
+      productIssue.status.Name,
+      productIssue.status.Value,
+      productIssue.issueMedias.Select(m => m.mediaUrl),
+      productIssue.returnDate,
+      productIssue.returnReason,
+      productIssue.isWarranty,
+      productIssue.customerEmail,
+      productIssue.customerFullname,
+      productIssue.customerPhonenumber,
+      productIssue.series,
+      productIssue.issueStateTrackings.Select(ProductIssueStateTrackingRecord.FromEntity)
       );
   }
 }
