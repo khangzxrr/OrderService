840d4a0c4fb614d163e1623739f779bba2f6d30e
khangzxrr <khangzxrr@gmail.com>
2023-06-02 14:37:00 +0700

    add shipping and new states

---
+++ src/OrderService.Core/ProductIssueAggregate/ProductIssueShipping.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Ardalis.GuardClauses;
+using OrderService.Core.OrderShippingAggregate;
+using OrderService.Core.ShipperAggregate;
+using OrderService.SharedKernel;
+
+namespace OrderService.Core.ProductIssueAggregate;
+public class ProductIssueShipping : EntityBase
+{
+  public Shipper shipper { get; private set; }
+
+  public OrderShippingStatus shippingStatus { get; private set; }
+
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+  public ProductIssueShipping()
+  {
+
+    shippingStatus = OrderShippingStatus.inWarehouse;
+  }
+
+  public void SetShipper(Shipper shipper)
+  {
+    this.shipper = Guard.Against.Null(shipper);
+  }
+
+  public void SetShippingStatus(OrderShippingStatus shippingStatus)
+  {
+    this.shippingStatus = Guard.Against.Null(shippingStatus);
+  }
+
+}
--- src/OrderService.Core/ProductIssueAggregate/ProductIssueStatus.cs
+++ src/OrderService.Core/ProductIssueAggregate/ProductIssueStatus.cs
@@ -5,13 +5,34 @@ public class ProductIssueStatus : SmartEnum<ProductIssueStatus>
 {
   public static ProductIssueStatus request = new(nameof(request), 0);
 
-  public static ProductIssueStatus acceptReturnEmployeeFault = new(nameof(acceptReturnEmployeeFault), 2);
-  public static ProductIssueStatus acceptReturnCustomerFault = new(nameof(acceptReturnCustomerFault), 3);
-  public static ProductIssueStatus acceptReturnSellerFault = new(nameof(acceptReturnSellerFault), 4);
+  public static ProductIssueStatus acceptEmployeeFault = new(nameof(acceptEmployeeFault), 2);
+  public static ProductIssueStatus acceptCustomerFault = new(nameof(acceptCustomerFault), 3);
+  public static ProductIssueStatus acceptSellerFault = new(nameof(acceptSellerFault), 4);
+
+  public static ProductIssueStatus exchangeforNew = new(nameof(exchangeforNew), 6);
+
+  public static ProductIssueStatus sentToSeller = new(nameof(sentToSeller), 7);
+
+  public static ProductIssueStatus successExchangeReturnToVN = new(nameof(successExchangeReturnToVN), 8);
+
+  public static ProductIssueStatus failedExchangeSellerRejectReturnToVN = new(nameof(failedExchangeSellerRejectReturnToVN), 9);
+
+
+  public static ProductIssueStatus refund = new(nameof(refund), 5);
+
+
+  public static ProductIssueStatus shippingToCustomer = new(nameof(shippingToCustomer), 10);
+
+  public static ProductIssueStatus shipperReceived = new(nameof(shipperReceived), 11);
+
+  public static ProductIssueStatus customerReceived = new(nameof(customerReceived), 12);
+
 
   public static ProductIssueStatus finish = new(nameof(finish), 99);
 
+
+
   public ProductIssueStatus(string name, int value) : base(name, value)
-  {
+  {    
   }
 }
