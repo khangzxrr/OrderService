1b34735a3eef6b1304c0b2d7ba1cfd803d977206
khangzxrr <khangzxrr@gmail.com>
2023-06-02 14:35:20 +0700

    new spec

---
+++ src/OrderService.Core/ProductIssueAggregate/specifications/ProductIssueByShipperIdSpec.cs
@@ -0,0 +1,21 @@
+﻿
+using Ardalis.Specification;
+using OrderService.Core.ProductReturnAggregate;
+
+namespace OrderService.Core.ProductIssueAggregate.specifications;
+public class ProductIssueByShipperIdSpec : Specification<ProductIssue>
+{
+  public ProductIssueByShipperIdSpec(int shipperId)
+  {
+    Query
+      .Include(pr => pr.product)
+        .ThenInclude(p => p.productCategory)
+      .Include(pr => pr.issuePayments)
+      .Include(pr => pr.issueStateTrackings)
+      .Include(pr => pr.productIssueShipping)
+
+      .Where(pr => (pr.productIssueShipping != null) && (pr.productIssueShipping.shipper.Id == shipperId));
+
+
+  }
+}
