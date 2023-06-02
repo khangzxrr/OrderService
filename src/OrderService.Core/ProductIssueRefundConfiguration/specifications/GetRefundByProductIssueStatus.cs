71dc0371965cdfe6931401a532dc911be5de4801
khangzxrr <khangzxrr@gmail.com>
2023-06-02 14:37:09 +0700

    spec

---
+++ src/OrderService.Core/ProductIssueRefundConfiguration/specifications/GetRefundByProductIssueStatus.cs
@@ -0,0 +1,13 @@
+﻿
+using Ardalis.Specification;
+using OrderService.Core.ProductReturnAggregate;
+
+namespace OrderService.Core.ProductIssueRefundConfiguration.specifications;
+public class GetRefundByProductIssueStatus : Specification<ProductIssueRefundConfiguration>, ISingleResultSpecification
+{
+  public GetRefundByProductIssueStatus(ProductIssueStatus productIssueStatus)
+  {
+    Query
+      .Where(refund => refund.productIssueStatus == productIssueStatus);
+  }
+}
