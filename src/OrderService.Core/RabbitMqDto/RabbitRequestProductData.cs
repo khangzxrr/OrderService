4f70c1b5407d21859e2d729f123d8a4a04e352f5
khangzxrr <khangzxrr@gmail.com>
2023-06-03 05:38:05 +0700

    remove product quantity

--- src/OrderService.Core/RabbitMqDto/RabbitRequestProductData.cs
+++ src/OrderService.Core/RabbitMqDto/RabbitRequestProductData.cs
@@ -6,12 +6,9 @@ public class RabbitRequestProductData
   public string connectionId { get; set; }
   public string productUrl { get; set; }
 
-  public int productQuantity { get; set; }
-
-  public RabbitRequestProductData(string connectionId, string productUrl, int productQuantity)
+  public RabbitRequestProductData(string connectionId, string productUrl)
   {
     this.connectionId = connectionId;
     this.productUrl = productUrl;
-    this.productQuantity = productQuantity;
   }
 }
