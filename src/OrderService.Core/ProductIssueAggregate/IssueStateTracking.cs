8ef8ce1fc5bd63b858d0967c058080e1062b28d3
khangzxrr <khangzxrr@gmail.com>
2023-06-02 14:35:56 +0700

    add issue tracking

---
+++ src/OrderService.Core/ProductIssueAggregate/IssueStateTracking.cs
@@ -0,0 +1,23 @@
+﻿
+using Ardalis.GuardClauses;
+using OrderService.Core.ProductReturnAggregate;
+using OrderService.SharedKernel;
+
+namespace OrderService.Core.ProductIssueAggregate;
+public class IssueStateTracking : EntityBase
+{
+  public ProductIssueStatus productIssueStatus { get; private set; }
+  public DateTime changeDate { get; private set; }
+
+  public IssueStateTracking(ProductIssueStatus productIssueStatus)
+
+  {
+    this.changeDate = DateTime.Now;
+    this.productIssueStatus = productIssueStatus;
+  }
+
+  public void SetProductIssueStatus(ProductIssueStatus productIssueStatus)
+  {
+    this.productIssueStatus = Guard.Against.Null(productIssueStatus);
+  }
+}
